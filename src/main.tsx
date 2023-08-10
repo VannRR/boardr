8ae32595296f876ea122f6a1785027fae0ae4deb
VannRR <92082133+VannRR@users.noreply.github.com>
2023-08-10 04:20:44 -0700

    added website files

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-import { render } from "preact";
-
-import { App } from "./app";
-
-import "./index.css";
-
-render(<App />, document.getElementById("app") as HTMLElement);
